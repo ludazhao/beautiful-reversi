cac79baa1c280d641fe34db63b581d8211a89bcc
Luda Zhao <94luda@gmail.com>
2015-12-22 16:16:49 -0800

    fixed port problem

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var game_server = require('./game_server');
 app.use(express.static(path.join(__dirname,'public')));
 
 // Create a Node.js based http server on port 8080
-var server = require('http').createServer(app).listen(8080);
+var server = require('http').createServer(app).listen(process.env.PORT ||8080);
 
 // Create a Socket.IO server and attach it to the http server
 var io = require('socket.io').listen(server);
